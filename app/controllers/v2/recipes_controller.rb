639574d19fcc7665f2ec0456b04de870021efd76
Brian <riceb53@gmail.com>
2018-03-08 19:35:02 -0600

    make show action

--- app/controllers/v2/recipes_controller.rb
+++ app/controllers/v2/recipes_controller.rb
@@ -6,4 +6,13 @@ class V2::RecipesController < ApplicationController
     # show the recipes to the user
     render json: recipes.as_json
   end
+
+  def show
+    # get user input
+    the_id = params[:id]
+    # grab a particular recipe from the db
+    recipe = Recipe.find_by(id: the_id)
+    # show that recipe to the user
+    render json: recipe.as_json
+  end
 end
