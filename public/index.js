925002aa7c12b86391830927a025e04c3dd70a34
Brian <riceb53@gmail.com>
2018-04-09 20:41:34 -0500

    use API request to dynamically get data from db

--- public/index.js
+++ public/index.js
@@ -3,13 +3,26 @@ console.log('the js file is being loaded correctly');
 var recipeTemplate = document.querySelector('#recipe-card');
 var recipeContainer = document.querySelector('.row');
 
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
 
 
+/* global axios */
+
+// response = Unirest.get("http://localhost:3000/v2/recipes")
+// p response.body
+// get data
+axios.get("http://localhost:3000/v2/recipes").then(function(response) {
+  console.log(response.data);
+  var recipes = response.data;
+  // loop through data
+  for (var i = 0; i < recipes.length; i++) {
+    var recipeClone = recipeTemplate.content.cloneNode(true);
+    recipeClone.querySelector('.card-title').innerText = recipes[i].title;
+    recipeClone.querySelector('.ingredients').innerText = recipes[i].ingredients;
+    recipeClone.querySelector('.directions').innerText = recipes[i].directions;
+    recipeClone.querySelector('.chef').innerText = recipes[i].chef;
+    recipeClone.querySelector('.image-url').src = recipes[i].image_url;
+    recipeContainer.appendChild(recipeClone);
+  }
+
+  // modify the clone
+});
